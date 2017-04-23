c6d7f977feb9a78150f6d62891ac92c0706dc2e9
Italo Nascimento <italo.abnascimento@gmail.com>
2017-04-23 14:07:40 -0300

    switching mongodb for bookshelf

---
+++ bookshelf.js
@@ -0,0 +1,8 @@
+var knex = require('knex')(require('knexfile.js'))
+
+const bookshelf = require('bookshelf')(knex)
+
+bookshelf.plugin(require('bookshelf-uuid'))
+bookshelf.plugin(require('bookshelf-paranoia'))
+
+module.export = bookshelf
---
+++ knexfile.js
@@ -0,0 +1,10 @@
+model.export = {
+  client: 'postgree',
+  connection: {
+    host     : '127.0.0.1',
+    user     : 'root',
+    password : '',
+    database : 'ape-rest',
+    charset  : 'utf8'
+  }
+}
