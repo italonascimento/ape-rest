e43a909c35df747d8d8944668e4fe319c6901ef4
Italo Nascimento <italo.abnascimento@gmail.com>
2017-04-30 19:57:26 -0300

    Created db seed for basic fields

---
+++ app/seeds/basic_fields.js
@@ -0,0 +1,21 @@
+const uuid = require('uuid/v4')
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('fields_types').del()
+    .then(function() {
+      return knex('fields').del()
+    })
+    .then(function() {
+      return knex('types').del()
+    })
+    .then(function () {
+      return knex('fields').insert([
+        {id: uuid(), name: 'Singleline', slug: 'singleline'},
+        {id: uuid(), name: 'Multiline', slug: 'multiline'},
+        {id: uuid(), name: 'Single choice', slug: 'single-choice'},
+        {id: uuid(), name: 'Multichoice', slug: 'multichoice'},
+        {id: uuid(), name: 'Dropdown', slug: 'dropdown'},
+      ]);
+    });
+};
